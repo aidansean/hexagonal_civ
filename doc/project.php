fe56a3524c9ea6d938d7a0791923f7b940a6620b
aidansean <aidan.randleconde@gmail.com>
2015-08-02 20:42:02 +0200

    Added images, reorganised directory structure

--- doc/project.php
+++ doc/project.php
@@ -1,4 +1,6 @@
 <?php
+include_once($_SERVER['FILE_PREFIX']."/project_list/project_object.php") ;
 $github_uri   = "https://github.com/aidansean/hexagonal_civ" ;
 $blogpost_uri = "http://aidansean.com/projects/?tag=hexagonal_civ" ;
+$project = new project_object("hexagonal_civ", "Hexagonal civilisation game", "https://github.com/aidansean/hexagonal_civ", "http://aidansean.com/projects/?tag=hexagonal_civ", "hexagonal_civ/images/project.jpg", "hexagonal_civ/images/project_bw.jpg", "Ever since I played Sid Meier's original <a href=\"http://en.wikipedia.org/wiki/Civilization_(video_game)\">Civilisation</a> I've wanted to make something similar to explore the gameplay and create something fun to pass the time.", "Games", "canvas,HTML,JavaScript") ;
 ?>
--- project.py
+++ project.py
@@ -3,11 +3,14 @@ from project_module import project_object, image_object, link_object, challenge_
 p = project_object('hexagonal_civ', 'Hexagonal civilisation game')
 p.domain = 'http://www.aidansean.com/'
 p.path = 'hexagonal_civ'
-p.preview_image_ = image_object('http://placekitten.com.s3.amazonaws.com/homepage-samples/408/287.jpg', 408, 287)
+p.preview_image    = image_object('%s/images/project.jpg'   %p.path, 150, 250)
+p.preview_image_bw = image_object('%s/images/project_bw.jpg'%p.path, 150, 250)
 p.folder_name = 'aidansean'
 p.github_repo_name = 'hexagonal_civ'
 p.mathjax = False
-p.links.append(link_object(p.domain, 'guess_the_song/', 'Live page'))
+p.tags = 'Games'
+p.technologies = 'canvas,HTML,JavaScript'
+p.links.append(link_object(p.domain, 'hexagonal_civ/', 'Live page'))
 p.introduction = 'Ever since I played Sid Meier\'s original <a href="http://en.wikipedia.org/wiki/Civilization_(video_game)">Civilisation</a> I\'ve wanted to make something similar to explore the gameplay and create something fun to pass the time.'
 p.overview = '''In principle this game should be fairly easy to develop because it is turn based and each problem (except the AI) is well defined.  I first tried developing a game using SVG and one of my friends suggested using hexagons, which gives it a nicer, more rounded feel.  However like many other projects this one is much lower priority than the others and it's hard to justify the large amount of time required for development, so it's stayed in a very rudimentary state for a long time.
 
